aadcb7cfdb8c99e98b12cbd5bc0a25ff039fea43
齋藤 雄太 <y.s.spector.bass@gmail.com>
2021-11-29 13:32:15 +0900

    feat: create fv page

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -82,6 +82,27 @@ using glitch_sample.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 11 "C:\Users\ysspe\Documents\WorkSpace\glitch-sample\_Imports.razor"
+using glitch_sample.Components.Template;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "C:\Users\ysspe\Documents\WorkSpace\glitch-sample\_Imports.razor"
+using glitch_sample.Components.Molecules;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\ysspe\Documents\WorkSpace\glitch-sample\_Imports.razor"
+using glitch_sample.Components.Atoms;
+
+#line default
+#line hidden
+#nullable disable
     public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
@@ -90,7 +111,7 @@ using glitch_sample.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 18 "C:\Users\ysspe\Documents\WorkSpace\glitch-sample\Shared\NavMenu.razor"
+#line 23 "C:\Users\ysspe\Documents\WorkSpace\glitch-sample\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
